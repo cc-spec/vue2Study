a512a1dfd60caf2425e8257b4c9760c44a89cbeb
caofang <15719192636@163.com>
2023-03-07 20:54:48 +0800

    更新

---
+++ basic/src/components/ModelItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      @change="$emit('change', $event.target.checked)"
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "ModelItem",
+  props: {
+    checkd: Boolean
+  }
+};
+</script>
---
+++ basic/src/views/HomeView.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <ModelItem v-model="changeItem"></ModelItem>
+  </div>
+</template>
+
+<script>
+import ModelItem from '@/components/ModelItem.vue';
+
+export default {
+  name: "HomeView",
+  comments: {
+    ModelItem
+  },
+  data() {
+    return {
+      changeItem: false
+    }
+  }
+};
+</script>
